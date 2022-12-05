64b063cabcfd73046e2c84c1d63d34094211533d
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-05 19:10:49 +0330

    add palette, typography, spacing, font family

--- styles/theme/theme.ts
+++ styles/theme/theme.ts
@@ -2,8 +2,47 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
-      
+        primary:{
+            main:'#F01436',
+        },
+        secondary:{
+            main:'#02a0b8',
+        },    
     },
+    typography:{
+        fontFamily: 'IRANSans',
+        h1:{
+            fontSize: 20,
+            fontWeight: 500,
+            color:'rgb(54, 54, 54)',
+        },
+        h2:{
+            fontSize: 18,
+            fontWeight: 400,
+            color:'rgb(54, 54, 54)',
+        },
+        subtitle1:{
+            fontSize: 16,
+            fontWeight: 500,
+            color:'rgb(0, 0, 0, 0,.87)',
+        },
+        subtitle2:{
+            fontSize: 14,
+            fontWeight: 400,
+            color:'rgb(54, 54, 54)',
+        },
+        button:{
+            fontSize: 14,
+            fontWeight: 400,
+            color: 'rgb(54, 54, 54)',
+        },
+        caption:{
+            fontSize: 12,
+            fontWeight: 400,
+            color: 'rgb(104, 104, 104)',
+        },
+    },
+    spacing: [0, 4, 8, 16, 32, 64],
 });
 
 export default theme;
