6256b1e521bfe92bbf234265dd82a169385243d2
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-05 15:12:02 +0330

    Change theme folder location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles';
-import theme from '../theme/theme';
+import theme from '../styles/theme/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
