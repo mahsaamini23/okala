356846741c10b90d506f50b03200c08ad6fdfe79
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-06 11:01:16 +0330

    add database

---
+++ data/AdsSlider/AdsSlider.ts
@@ -0,0 +1,18 @@
+import image1 from  '../../assets/image/399251.jpeg';
+import image2 from  '../../assets/image/399283.jpeg';
+import image3 from  '../../assets/image/399286.jpeg';
+import image4 from  '../../assets/image/399287.jpeg';
+import image5 from  '../../assets/image/399280.jpeg';
+import image6 from  '../../assets/image/399411.jpeg';
+import image7 from  '../../assets/image/399413.jpeg';
+
+
+export const Ads=[
+    {id:1, image:{image1}},
+    {id:2, image:{image2}},
+    {id:3, image:{image3}},
+    {id:4, image:{image4}},
+    {id:5, image:{image5}},
+    {id:6, image:{image6}},
+    {id:7, image:{image7}},
+]
