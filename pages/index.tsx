72890a9f5a5dcb1212eecd416341e6dbba2c683b
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-07 18:42:05 +0330

    add downloadApp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import AdsSlider from '../component/main/AdsSlider/Slider';
 import StoreCard from '../component/main/StoreCard/StoreCard';
-
+import DownloadApp from '../component/main/DownloadApp/DownloadApp';
 export default function Home() {
   return (
 
     <div  dir="rtl">
       <AdsSlider/>
       <StoreCard/>
-
-
+      <DownloadApp/>
     </div>
   )
 }
