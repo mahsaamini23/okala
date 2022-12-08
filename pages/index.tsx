1e0bcf75d9907f83c2f9802df5bf2878ef242bfc
Ali <mrruler1340@gmail.com>
2022-12-08 05:17:38 +0330

    complete them

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-// import AdsSlider from '../component/main/AdsSlider/Slider';
+import AdsSlider from '../component/main/AdsSlider/Slider';
 import StoreCard from '../component/main/StoreCard/StoreCard';
 
 export default function Home() {
   return (
     <div>
-      {/* home
-      <AdsSlider/> */}
+
+
+      <AdsSlider/>
       <StoreCard/>
+
+
     </div>
   )
 }
