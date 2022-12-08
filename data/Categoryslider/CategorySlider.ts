4082cb6950d67402667d094ae40918a7bb5fdc00
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-08 18:14:04 +0330

    add image and data for categorySlider

---
+++ data/Categoryslider/CategorySlider.ts
@@ -0,0 +1,22 @@
+import picture1 from "../../assets/image/1m.png";
+import picture2 from "../../assets/image/2m.png";
+import picture3 from "../../assets/image/3m.png";
+import picture4 from "../../assets/image/4m.png";
+import picture5 from "../../assets/image/5m.png";
+import picture6 from "../../assets/image/6m.png";
+import picture7 from "../../assets/image/7m.png";
+import picture8 from "../../assets/image/8m.png";
+
+
+const category = [
+    {id:1, name:"خواربار", background:'#c3f3c1', image:picture1},
+    {id:2, name:"لبنیات", background:'#98c6f6', image:picture2},
+    {id:3, name:"محصولات پروتیینی", background:'#f8d3ca', image:picture3},
+    {id:4, name:"کنسرو وغذای آماده", background:'#f89349', image:picture4},
+    {id:5, name:"نوشیدنی ها", background:'#51e0e7', image:picture5},
+    {id:6, name:"صبحانه", background:'#eeb7e4', image:picture6},
+    {id:7, name:"تنقلات", background:'#b8e6d8', image:picture7},
+    {id:8, name:"آجیل و خشکبار", background:'#c1d0ff', image:picture8},
+]
+
+export default category;
