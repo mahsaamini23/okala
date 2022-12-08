ff5f0b47c4ec21d3d8c309c067fcdc8e20c3121d
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-08 16:47:45 +0330

    add Download app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
+import Grid from '@mui/material/Grid';
 import AdsSlider from '../component/main/AdsSlider/Slider';
 import StoreCard from '../component/main/StoreCard/StoreCard';
 import DownloadApp from '../component/main/DownloadApp/DownloadApp';
 export default function Home() {
   return (
 
-    <div  dir="rtl">
+    <Grid  dir="rtl" >
       <AdsSlider/>
       <StoreCard/>
       <DownloadApp/>
-    </div>
+    </Grid>
   )
 }
 
