ae18d0c0f73c2480acbf57fb707e9408ebee5687
baran-aa77 <rezvan.a78@gmail.com>
2022-12-09 10:53:48 +0330

    store cards updated

--- component/main/StoreCards-B/StoreCard-B/StoreCard-B.tsx
+++ component/main/StoreCards-B/StoreCard-B/StoreCard-B.tsx
@@ -1,30 +1,47 @@
 import { Grid, Typography } from "@mui/material"
 import Image from "next/image"
-import pic1 from '../../../../assets/image/koorosh.webp'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-const StoreCard=()=>{
-    return(
-<Grid container item xs={12}md={4} sx={{border:'1px solid lightgray',borderRadius:'3px',padding:'10px'}}>
-    <Grid item xs={3} sx={{border:'1px solid lightgray',borderRadius:'3px',display:'flex',justifyContent:'center',alignSelf:'center',padding:'10px'}}>
-    <Image src={pic1} alt='logo' style={{width:'80px',height:'80px'}}/>
-    </Grid>
-   <Grid container item xs={9} sx={{padding:'10px',gap:'8px'}}>
-    <Grid item xs={12}>
-        <Typography variant='h2'>افق کوروش</Typography>
-    </Grid>
-    <Grid container item xs={12}>
-        <Grid xs={10}>
-        <Typography>4.45</Typography>
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import star from '../../../../assets/image/star.png'
+type props={
+    img:string,
+    title:string,
+    rate:string,
+    subtitle:string,
+}
+const StoreCard = ({img,title,rate,subtitle}:props) => {
+    return (
+        <Grid container item xs={12} md={5} sx={{ border: '1px solid #e0e0e0', borderRadius: '3px', padding: '10px','&:hover':{boxShadow:'1'}}}>
+            <Grid item xs={3} sx={{ border: '1px solid #e0e0e0', borderRadius: '3px', display: 'flex', justifyContent: 'center', alignSelf: 'center', padding: '10px' }}>
+                <Image src={img} alt='logo' style={{ width: '80px', height: '80px' }} />
+            </Grid>
+            <Grid container item xs={9} sx={{ padding: '10px', gap: '8px' }}>
+                <Grid item xs={12}>
+                    <Typography variant='h2' sx={{fontWeight: 'bold'}}>{title}</Typography>
+                </Grid>
+                <Grid container item xs={12}>
+                    <Grid xs={10} container item alignItems='center'>
+                        <Grid item xs={1}>
+                        <Image src={star} alt='star' style={{width:'20px',height:'20px'}}></Image>
+                        </Grid>
+                        <Grid item xs={11}>
+                        <Typography variant='subtitle1'>{rate}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'flex-end',cursor:'pointer',color:'#757575' }}>
+                        <ArrowBackIosIcon fontSize="small"/>
+                    </Grid>
+                </Grid>
+                <Grid container item xs={12} alignItems='center'>
+                    <Grid item xs={1}>
+                    <AccessTimeIcon fontSize="small"/>
+                    </Grid>
+                    <Grid item xs={11}>
+                    <Typography variant='subtitle1'>{subtitle}</Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Grid>
-    <Grid item xs={2} sx={{display:'flex',justifyContent:'flex-end'}}>
-        <ArrowBackIosIcon/>
-    </Grid>
-    </Grid>
-    <Grid item xs={12}>
-    <Typography>تحویل در 45 دقیقه</Typography>
-    </Grid>
-   </Grid>
-</Grid>
     )
 }
 export default StoreCard
--- component/main/StoreCards-B/StoreCards.tsx
+++ component/main/StoreCards-B/StoreCards.tsx
@@ -1,12 +1,14 @@
 import { Grid } from "@mui/material"
 import StoreCard from "./StoreCard-B/StoreCard-B"
-
+import pic1 from '../../../assets/image/koorosh.webp'
+import pic2 from '../../../assets/image/omarket.webp'
+import pic3 from '../../../assets/image/obinsness.webp'
 const StoreCards=()=>{
     return(
 <Grid container xs={12} sx={{gap:'10px'}}>
-    <StoreCard></StoreCard>
-    <StoreCard></StoreCard>
-    <StoreCard></StoreCard>
+    <StoreCard img={pic1} title={'کوروش'} rate={'4.45'} subtitle={'تحویل در 45 دقیقه'}></StoreCard>
+    <StoreCard img={pic2} title={'امارکت'} rate={'3'} subtitle={'تحویل در 60 دقیقه'}></StoreCard>
+    <StoreCard img={pic3} title={'ابیزینس'} rate={'4.45'} subtitle={'تحویل در 15 دقیقه'}></StoreCard>
 </Grid>
     )
 }
