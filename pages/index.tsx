27fbf6eaf7069ab63f5a7430f69a0145c64575f2
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-10 13:29:48 +0330

    Add categorySlider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import AdsSlider from '../component/main/AdsSlider/Slider';
-import StoreCard from '../component/main/StoreCard/StoreCard';
+import CategorySlider from '../component/main/CategorySlide/CategorySlider';
 import Footer from '../component/main/Footer/Footer'
 import Category from '../component/main/CategorySlide/Category/Category';
 export default function Home() {
+
   return (
     <div>
-<AdsSlider/>
-<StoreCard/>
-<Category/>
-<Footer/>
+      <AdsSlider/>
+      <CategorySlider/>
+      <Footer/>
     </div>
   )
 }
