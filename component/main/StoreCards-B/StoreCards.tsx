3b9a3f45d82e1b91fdb24b0d25b96b099d8e66f0
Ali <mrruler1340@gmail.com>
2022-12-10 12:03:41 +0330

    doing__description__aliforuz__created description component

--- component/main/StoreCards-B/StoreCards.tsx
+++ component/main/StoreCards-B/StoreCards.tsx
@@ -3,7 +3,7 @@ import StoreCard from "./StoreCard-B/StoreCard-B"
 
 const StoreCards=()=>{
     return(
-<Grid container xs={12} sx={{gap:'10px'}}>
+<Grid container xs={12} sx={{gap:'20px' , margin:'3em 0'}}>
     <StoreCard></StoreCard>
     <StoreCard></StoreCard>
     <StoreCard></StoreCard>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,25 @@ import Footer from '../component/main/Footer/Footer'
 import Category from '../component/main/CategorySlide/Category/Category';
 import SearchBar from '../component/main/SearchBar/SearchBar'
 import StoreCards from '../component/main/StoreCards-B/StoreCards';
+import Container from '@mui/material/Container';
+import SimpleAccordion from '../component/main/describtion/des';
 
 export default function Home() {
   return (
-
+    
     <Grid>
-      <SearchBar/>
+      <Container maxWidth="lg">
+          <SearchBar/>  
+      </Container>
       <AdsSlider/>
+      <Container maxWidth="lg">
       <Category/>
       <StoreCards/>
+      <SimpleAccordion />
       <Footer/>
+      </Container>
+      
+
 
     </Grid>
   )
