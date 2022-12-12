2234ee0a4edfa105788b9dcbd2189673b4d96b26
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-12 18:04:46 +0330

    create page categories

---
+++ pages/browse/category.tsx
@@ -0,0 +1,14 @@
+import { GetStaticPaths } from "next";
+import params from "../../data/CategorySlider/CategorySlider";
+
+
+const Categories =()=>{
+    return(
+        <div>
+            Categories
+        </div>
+    )
+}
+
+export default Categories;
+
