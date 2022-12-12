0c6694850707a0a267c659040fbbbb7b7395d13d
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-12 18:04:12 +0330

    delete card category

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import AdsSlider from '../component/main/AdsSlider/Slider';
+import AdsSlider from '../component/main/AdsSlider/AdsSlider';
 import CategorySlider from '../component/main/CategorySlide/CategorySlider';
 import Footer from '../component/main/Footer/Footer'
 import Category from '../component/main/CategorySlide/Category/Category';
