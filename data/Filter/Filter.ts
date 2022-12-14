e38afbb8660ef1f46c1ca56ca0ecd2e47249577a
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-14 11:26:53 +0330

    add data for barnd filter

---
+++ data/Filter/Filter.ts
@@ -0,0 +1,11 @@
+export const brand = [
+    {id:1, name:'خشک پاک'},
+    {id:2, name:'گلستان'},
+    {id:3, name:'پردیس'},
+    {id:4, name:'گلها'},
+    {id:5, name:'سمیه'},
+    {id:6, name:'سه نان'},
+    {id:7, name:'فامیلا'},
+    {id:8, name:'زر'},
+    {id:9, name:'اویلا'},
+]
