06a7e4473b44008633a3d0b6c1c7d07b8b6cfa66
Mohamad Hossein Moghaddamfar <mhmoghadamfar@gmail.com>
2022-12-17 18:34:44 +0330

    Success-Config Mirage-Moghaddamfar- Mirage config created in server Folder and there is a example use it as a user Login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 import CategorySlider from '../component/main/CategorySlide/CategorySlider';
 import AdsSlider from '../component/main/AdsSlider/AdsSlider';
 import Footer from '../component/main/Footer/Footer';
@@ -9,26 +10,31 @@ import Describtion from '../component/main/describtion/describtion';
 import Banner from '../component/main/banner/banner';
 import Banner1 from '../assets/image/green.webp'
 import Banner2 from '../assets/image/orange.webp'
-import ProductCard from '../component/common/ProductCard/ProductCard';
 import DownloadApp from '../component/main/DownloadApp/DownloadApp';
+import { getExample } from '../api/api';
+import { useEffect } from 'react';
+import Header from "../component/main/Header/header"
 
 
 export default function Home() {
 
+  useEffect(() => { getExample().then(res => console.log(res)); })
+
   return (
     <Grid>
       <Container maxWidth="lg">
-      <SearchBar/>  
+        <Header />
+        <SearchBar />
       </Container>
-      <AdsSlider/>
+      <AdsSlider />
       <Container maxWidth="lg">
-      <StoreCards/>
-      <CategorySlider/>
-      <Banner image={Banner1} />
-      <Banner image={Banner2} />
-      <Describtion />
-      <DownloadApp/>
-      <Footer/>
+        <StoreCards />
+        <CategorySlider />
+        <Banner image={Banner1} />
+        <Banner image={Banner2} />
+        <Describtion />
+        <DownloadApp />
+        <Footer />
       </Container>
     </Grid>
   )
