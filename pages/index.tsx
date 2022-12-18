1f1b078f3bb41cf8ed18ef5afb339f34322466dc
Mohamad Hossein Moghaddamfar <mhmoghadamfar@gmail.com>
2022-12-18 15:23:11 +0330

    Create-InitializeStructure-Moghaddamfar- Create database and Create page login

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     <Grid>
       <Container maxWidth="lg">
         <Header />
-        <SearchBar />
+        {/* <SearchBar /> */}
       </Container>
       <AdsSlider />
       <Container maxWidth="lg">
