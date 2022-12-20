ed76c5b5074e18a5055830d7716926aea769809f
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-20 11:06:01 +0330

    remove file footer, download app and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
 import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
-import CategorySlider from '../component/main/CategorySlide/CategorySlider';
-import AdsSlider from '../component/main/AdsSlider/AdsSlider';
-// import Footer from '../component/main/Footer/Footer';
-// import SearchBar from '../component/main/SearchBar/SearchBar';
-import StoreCards from '../component/main/StoreCards-B/StoreCards';
 import Container from '@mui/material/Container';
-import Describtion from '../component/main/describtion/describtion';
-import Banner from '../component/main/banner/banner';
-import Banner1 from '../assets/image/green.webp'
-import Banner2 from '../assets/image/orange.webp'
-// import DownloadApp from '../component/Layout/DownloadApp/DownloadApp';
+
 import { getExample } from '../api/api';
 import { useEffect } from 'react';
-import Header from "../component/main/Header/header"
+import { styled } from '@mui/material/styles';
+
+import CategorySlider from '../component/main/CategorySlide/CategorySlider';
+import AdsSlider from '../component/main/AdsSlider/AdsSlider';
+import Footer from '../component/Layout/Footer/Footer';
+import StoreCards from '../component/main/StoreCards/StoreCards';
+import Banner from '../component/main/banner/banner';
+import Header from '../component/Layout/Header/header';
 
 
 export default function Home() {
@@ -21,20 +18,16 @@ export default function Home() {
   useEffect(() => { getExample().then(res => console.log(res)); })
 
   return (
-    <Grid bgcolor={'#fafafa'}>
+    <Grid bgcolor={'#fafafa'} display={'flex'} flexDirection={'column'} justifyContent={'center'}>
       <Container maxWidth="lg">
         <Header />
-        {/* <SearchBar /> */}
-      </Container>
+      </Container >
       <AdsSlider />
-      <Container maxWidth="lg">
+      <Container sx={{borderBottom:'1px solid #757575', margin:'0 0 30px 0'}} >
         <StoreCards />
         <CategorySlider />
-        <Banner image={Banner1} />
-        <Banner image={Banner2} />
-        <Describtion />
-        {/* <DownloadApp /> */}
-        {/* <Footer /> */}
+        <Banner/>
+        <Footer/>
       </Container>
     </Grid>
   )
