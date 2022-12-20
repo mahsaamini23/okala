5b940794b266e69fadfd002b5de15347fc2ed393
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-20 11:04:47 +0330

    add data for store cards

---
+++ data/StoreCards/StoreCards.ts
@@ -0,0 +1,11 @@
+import img1 from "../../assets/image/koorosh.webp";
+import img2 from "../../assets/image/omarket.webp";
+import img3 from "../../assets/image/obinsness.webp";
+
+const infoStoreCards = [
+    {id:1, title:'کوروش', rate:'4.45', subtitle:'تحویل در ۴۵ دقیقه', image:img1},
+    {id:2, title:'امارکت', rate:'3', subtitle:'تحویل در ۶۰ دقیقه', image:img2},
+    {id:3, title:'ابیزینس', rate:'4.45', subtitle:'تحویل در ۱۵ دقیقه', image:img3},
+]
+
+export default infoStoreCards;
