94897965c145d7289ea246752502fbf32e7d71b0
ms.moradi23 <ms.moradi23@yahoo.com>
2023-01-01 22:17:05 +0330

    Update-brandFilter/PriceFilter/SwitchFilter-Moradi- update baxshadow for container

--- component/filter/SwitchFilter/SwitchFilter.tsx
+++ component/filter/SwitchFilter/SwitchFilter.tsx
@@ -1,14 +1,11 @@
 
 import Grid from '@mui/material/Grid';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 import StyledSwitch from '../../../styles/styledSwitch/styledSwitch';
 import Typography from '@mui/material/Typography';
 
 const SwitchFilter =()=>{
     return(
-        <Grid sx={{padding:{xs:'0px', md:'20px'}}} bgcolor={'#fff'} boxShadow={'0px 2px rgba(22 22 22 4%) !important'} borderRadius={'4px'} >
+        <Grid sx={{padding:{xs:'0px', md:'20px'}}} bgcolor={'#fff'} boxShadow={'0px 2px 10px 5px #e9e9e9'} borderRadius={'4px'} >
             <Grid container display={'flex'} alignItems={'center'} gap={'5px'} sx={{borderBottom:{xs:'1px solid #e9e9e9', md:'0'}, padding:{xs:'30px 0', md:'20px 0'}}}>
                 <StyledSwitch size='small'/>
                 <Typography variant='subtitle2'>فقط کالاهای موجود</Typography>
