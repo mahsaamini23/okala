5fdf7c210befaa8ac46ef0d6479b4b7d89df5871
ms.moradi23 <ms.moradi23@yahoo.com>
2023-01-02 22:55:47 +0330

    Update-categoryFilter-Moradi-add flex to grids

--- component/filter/CategoryFilter/CategoryFilter.tsx
+++ component/filter/CategoryFilter/CategoryFilter.tsx
@@ -8,12 +8,12 @@ const CategoryFilter =() : JSX.Element =>{
     const [list, setList] = useState([{id:1, title:'تخم مرغ'},{id:2, title:'ماهی'},{id:3, title:'گوشت گاو و گوساله'},{id:4, title:'گوشت مرغ'}])
     return(
         <Grid p={2} boxShadow={'0px 2px 10px 5px #e9e9e9'} bgcolor={'#fff'} borderRadius={'4px'}>
-            <Grid pr={2}>
+            <Grid pr={2} container display={'flex'} justifyContent={'flex-start'} gap={'5px'}>
                 <KeyboardArrowLeftIcon sx={{fontSize:20}}/>
                 <Typography variant='subtitle2'>پروتیین</Typography>
             </Grid>
             {list.map((item)=>(
-                <Grid key={item.id} pr={2}>
+                <Grid key={item.id} pr={4} container display={'flex'} justifyContent={'flex-start'} gap={'5px'}>
                     <KeyboardArrowLeftIcon  sx={{fontSize:20}} />
                     <Typography variant='subtitle2'>{item.title}</Typography>
                 </Grid>
