13c32ad9cdf4ec5202592262ff1d80c65f5ffbef
ms.moradi23 <ms.moradi23@yahoo.com>
2023-01-02 21:54:56 +0330

    Update-ProductCard-Moradi-add type and props and data

--- component/filter/Products/ProductCard/ProductCard.tsx
+++ component/filter/Products/ProductCard/ProductCard.tsx
@@ -5,23 +5,30 @@ import Image from 'next/image';
 import photo from "../../../../assets/image/Protein/2311.png"
 import Typography from "@mui/material/Typography";
 
-const ProductCard = () : JSX.Element =>{
+type Props ={
+    image: string|any,
+    price: number,
+    title: string,
+    order: number,
+}
+
+const ProductCard = ({image, price, title, order} : Props) =>{
     return (
-        <Grid width={'200px'} px={2} py={4} display={'flex'} flexDirection={'column'} gap={'10px'} border={'1px solid #e9e9e9'}> 
+        <Grid width={'200px'} height={'300px'} px={2} py={4} display={'flex'} flexDirection={'column'} gap={'10px'} border={'1px solid #e9e9e9'}> 
             <Grid mb={2} container display={'flex'} alignItems={'flex-end'} gap={'10px'}>
                 <ControlPointIcon sx={{color:"#f01436", fontSize:35, fontWeight:"light"}}/>
-                <Image src={photo} alt="product" style={{width:'100px', height:'100px',display:'flex', alignSelf:'center'}}/>
+                <Image src={image} alt="product" style={{width:'100px', height:'100px',display:'flex', alignSelf:'center'}}/>
             </Grid>
             <Grid container display={'flex'} justifyContent={'space-between'}>
                 <Grid >
-                    <Typography variant='subtitle2'>۱۴۵۰۰ ریال</Typography>
+                    <Typography variant='subtitle2'>{price} ریال</Typography>
                     <Typography variant='caption'> ۱۷۰۰۰</Typography>
                 </Grid>
                 <Grid width={'25px'} height={'20px'} bgcolor={'#f01436'} color={'#fff'}borderRadius={'5px'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                    <Typography variant='caption' color={'#fff'} >۱۲٪</Typography>
+                    <Typography variant='caption' color={'#fff'} >{order}٪</Typography>
                 </Grid>
             </Grid>
-            <Typography variant='subtitle2'>اسپاگتی ۱.۴ رشته ای ۵۰۰ گرمی مانا</Typography>
+            <Typography variant='subtitle2'>{title}</Typography>
         </Grid>
     )
 }
